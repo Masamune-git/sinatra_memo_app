99f7db5c7136769e20a02a2e50db10b7cf2ce859
gentosato <2396niisankyumu@gmail.com>
2021-04-08 10:47:44 +0900

    class Memo を定義

--- main.rb
+++ main.rb
@@ -1,28 +1,36 @@
+# frozen_string_literal: true
+
 require 'sinatra'
 require 'sinatra/reloader'
 require 'securerandom'
-memo_names = []
-memo_links = []
 
-get '/' do 
+memo_dates = []
+
+class Memo
+  attr_accessor :memo_name, :memo_id
+  def initialize(memo_name, memo_id)
+    @memo_name = memo_name
+    @memo_id = memo_id
+  end
+end
+
+get '/' do
   @title = 'top'
-  @memo_names = memo_names
+  @memo_dates = memo_dates
   erb :top
 end
 
-get '/new_memo' do 
+get '/new_memo' do
   @title = 'new_memo'
   erb :new_memo
 end
 
-post "/new_memo/add" do 
-  file_name = SecureRandom.uuid
-  File.open("db/#{file_name}","w") do |text|
-    text.puts( params[:memo_text] )
+post '/new_memo/add' do
+  memo_id = SecureRandom.uuid
+  File.open("db/#{memo_id}", 'w') do |text|
+    text.puts(params[:memo_text])
   end
-  
-  memo_names << params[:memo_name]
-  memo_links << "db/#{file_name}"
-  redirect '/'    
+  memo_dates << Memo.new(params[:memo_name].to_s, memo_id)
+  redirect '/'
   erb :index
 end
