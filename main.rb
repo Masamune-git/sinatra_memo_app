f9dbf0fd0836d91325a15af0432778e8c56f44d3
gentosato <2396niisankyumu@gmail.com>
2021-04-09 16:34:18 +0900

    ハッシュ を用いてデータを格納

--- main.rb
+++ main.rb
@@ -6,31 +6,43 @@ require 'securerandom'
 
 memo_dates = []
 
-class Memo
-  attr_accessor :memo_name, :memo_id
-  def initialize(memo_name, memo_id)
-    @memo_name = memo_name
-    @memo_id = memo_id
-  end
-end
-
 get '/' do
   @title = 'top'
   @memo_dates = memo_dates
   erb :top
 end
 
+get '/edit_memo/:id' do
+  @title = 'Edit memo'
+  @memo_date = memo_dates.find{|memo_date|
+  memo_date["memo_id"] == params[:id]
+  }
+  erb :edit_memo
+end
+
+get '/show_memo/:id' do
+  @title = 'Show memo'
+  @memo_date = memo_dates.find{|memo_date| 
+    memo_date["memo_id"] == params[:id]
+  }
+  erb :show_memo
+end
+
+get '/delete/:id' do
+  redirect '/'
+end
+
 get '/new_memo' do
-  @title = 'new_memo'
+  @title = 'New memo'
   erb :new_memo
 end
 
 post '/new_memo/add' do
-  memo_id = SecureRandom.uuid
-  File.open("db/#{memo_id}", 'w') do |text|
-    text.puts(params[:memo_text])
-  end
-  memo_dates << Memo.new(params[:memo_name].to_s, memo_id)
+  memo_date = {
+    "memo_name" => params[:memo_name],
+    "memo_id"=> SecureRandom.uuid,
+    "memo_text" => params[:memo_text]
+}
+  memo_dates << memo_date
   redirect '/'
-  erb :index
 end
--- test.rb
+++ test.rb
@@ -1,6 +1,13 @@
-require 'securerandom'
+memo_dates = []
 
-file_name = SecureRandom.uuid
-File.open("#{file_name}","w") do |text|
-  text.puts("Hello!!")
- end
+memo_name = 'a'
+memo_id = 1234
+memo_text = 'hoge'
+
+memo_date = { 'memo_name' => memo_name, 'memo_id' => memo_id, 'memo_text' => memo_text }
+
+memo_dates << memo_date
+
+p memo_dates.find { |memo_date|
+  memo_date['memo_id'] == memo_id
+}
