f935b4ae690d6b08218c0c41bd6b43dbf72d9d08
gentosato <2396niisankyumu@gmail.com>
2021-04-22 02:44:07 +0900

    jsonファイルへの読み書き、メモの編集、削除機能を追加

--- main.rb
+++ main.rb
@@ -3,32 +3,84 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'securerandom'
+require 'json'
 
 memo_dates = []
 
+class Memo
+  def self.write_json(memo_dates)
+    File.open('db/db.json', 'w') do |file|
+      file.puts(JSON.generate(memo_dates))
+    end
+  end
+
+  def self.read_json
+    json_dates = []
+    File.open('db/db.json', 'r') do |file|
+      json_dates = JSON.load(file)
+    end
+    json_dates = [] if json_dates.nil?
+    json_dates
+  end
+
+  def self.create_memo(memo_name, memo_id, memo_text)
+    {
+      'memo_name' => memo_name,
+      'memo_id' => memo_id,
+      'memo_text' => memo_text
+    }
+  end
+
+  def self.edit_memo(memo_name, memo_id, memo_text)
+    {
+      'memo_name' => memo_name,
+      'memo_id' => memo_id,
+      'memo_text' => memo_text
+    }
+  end
+end
+
 get '/' do
   @title = 'top'
-  @memo_dates = memo_dates
+  @memo_dates = Memo.read_json
   erb :top
 end
 
 get '/edit_memo/:id' do
   @title = 'Edit memo'
-  @memo_date = memo_dates.find{|memo_date|
-  memo_date["memo_id"] == params[:id]
-  }
+  @memo_dates = Memo.read_json
+  @memo_date = @memo_dates.find do |memo_date|
+    memo_date['memo_id'] == params[:id]
+  end
   erb :edit_memo
+end
+
+post '/edit_memo/edit_run/:id' do
+  @memo_dates = Memo.read_json
+  @edit_before = @memo_dates.find do |memo_date|
+    memo_date['memo_id'] == params[:id]
+  end
+  @edit_after = Memo.edit_memo(params[:memo_name], params[:id], params[:memo_text])
+  memo_dates = @memo_dates.map { |memo_date| memo_date == @edit_before ? @edit_after : memo_date }
+  Memo.write_json(memo_dates)
+  redirect '/'
 end
 
 get '/show_memo/:id' do
   @title = 'Show memo'
-  @memo_date = memo_dates.find{|memo_date| 
-    memo_date["memo_id"] == params[:id]
-  }
+  @memo_dates = Memo.read_json
+  @memo_date = @memo_dates.find do |memo_date|
+    memo_date['memo_id'] == params[:id]
+  end
   erb :show_memo
 end
 
 get '/delete/:id' do
+  @memo_dates = Memo.read_json
+  @memo_dates.delete_if  do |memo_date|
+    memo_date['memo_id'] == params[:id]
+  end
+  Memo.write_json(@memo_dates)
   redirect '/'
 end
 
@@ -38,11 +90,8 @@ get '/new_memo' do
 end
 
 post '/new_memo/add' do
-  memo_date = {
-    "memo_name" => params[:memo_name],
-    "memo_id"=> SecureRandom.uuid,
-    "memo_text" => params[:memo_text]
-}
-  memo_dates << memo_date
+  @memo_dates = Memo.read_json
+  @memo_dates << Memo.create_memo(params[:memo_name], SecureRandom.uuid, params[:memo_text])
+  Memo.write_json(@memo_dates)
   redirect '/'
 end
--- test.rb
+++ test.rb
@@ -1,13 +1,2 @@
-memo_dates = []
-
-memo_name = 'a'
-memo_id = 1234
-memo_text = 'hoge'
-
-memo_date = { 'memo_name' => memo_name, 'memo_id' => memo_id, 'memo_text' => memo_text }
-
-memo_dates << memo_date
-
-p memo_dates.find { |memo_date|
-  memo_date['memo_id'] == memo_id
-}
+array = [1,2,3,4,5,6]
+p array.map{|e| e % 2 == 0 ? e**2 : e}
